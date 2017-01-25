5e9290e71af801d7aaaa76e71433c085d9e529dd
Ivan Kutuzov <arbrix@gmail.com>
2017-01-25 23:17:49 +0200

    Example functions were moved to the test file

--- examples/min_test.go
+++ examples/min_test.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 	"testing"
 )
+
+func min(a, b int) int {
+	return int(math.Min(float64(a), float64(b)))
+}
+
+func min2(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 
 func BenchmarkMin(b *testing.B) {
 	a, c := rand.Int(), rand.Int()
